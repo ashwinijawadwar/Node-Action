d1c109975256ec7e8acfa0f40100abdcaedecd80
Ubuntu <ubuntu@ip-172-31-40-163.ap-south-1.compute.internal>
2025-03-08 20:06:11 +0000

    Nodejs CI/CD Using GitHub Action..git add .

--- index.js
+++ index.js
@@ -8,58 +8,18 @@
 //module.exports = app; // Ensure this is present
 
 //   222222222 wala code
-//const express = require('express');
-//const app = express();
-
-//app.get('/', (req, res) => {
-//    res.status(200).send('Hello, World!');
-//});
-
-//const PORT = 3000;
-//app.listen(PORT, () => {
-//    console.log(`Server is running on http://localhost:${PORT}`);
-//});
-
-//module.exports = app;
-
-
-/// 333 wala code
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send(`
-        <html>
-            <head>
-                <title>Cool Hello World</title>
-                <style>
-                    body {
-                        font-family: Arial, sans-serif;
-                        text-align: center;
-                        margin-top: 50px;
-                        background-color: #282c34;
-                        color: #61dafb;
-                    }
-                    h1 {
-                        font-size: 50px;
-                        text-shadow: 2px 2px 10px rgba(255, 255, 255, 0.5);
-                    }
-                </style>
-            </head>
-            <body>
-                <h1>❤️ Heyyy Aaishhhhh...!! ❤️</h1>
-                <p>Much love to you from <b>Prasad</b> ❤️</p>
-            </body>
-        </html>
-    `);
+    res.status(200).send('Hello, World!');
 });
 
-if (require.main === module) {
-    const PORT = 3000;
-    app.listen(PORT, () => {
-        console.log(`🚀 Server is running on http://localhost:${PORT}`);
-    });
-}
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
+
 
