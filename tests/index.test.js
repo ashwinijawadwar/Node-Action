2f0fd46967e9693cbd2e54b51063289627049390
Ubuntu <ubuntu@ip-172-31-40-163.ap-south-1.compute.internal>
2025-03-08 17:33:37 +0000

    ellaaaaaaaaa

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const express = require('express');
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello, GitHub Actions!'));
+app.get('/', (req, res) => res.send('Hello, GitHub Actions.!'));
 
-test('GET / should return Hello, GitHub Actions!', async () => {
+test('GET / should return Hello, GitHub Actions.!', async () => {
     const res = await request(app).get('/');
-    expect(res.text).toBe('Hello, GitHub Actions!');
+    expect(res.text).toBe('Hello, GitHub Actions.!');
 });
